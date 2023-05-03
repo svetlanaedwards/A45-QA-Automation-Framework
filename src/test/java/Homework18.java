a8c17a66c14de3ec85366d29f4188e2606ec1919
svetlanaedwards <131229677+svetlanaedwards@users.noreply.github.com>
2023-05-02 20:21:46 -0600

    HM18
    I can't figure out what's wrong. Driver. highlighted in red and i think my HM can't extend BaseTest file and use methods from there

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+
+public class Homework18 extends BaseTest {
+
+@Test
+
+ public void playASongTest() throws InterruptedException {
+     login ("svetlana.edwards@testpro.io", "te$t$tudent");
+     playSong();
+     Assert.assertTrue(isSongPlaying());
+ }
+
+ public void playSong() {
+     WebElement playNextButton = driver.findelement(By.xpath("//i[@data-testid='play-next-btn']"));
+     WebElement playButton = driver.findelement(By.xpath("//span[@data-testid='play-btn']"));
+
+     playNextButton.click();
+     playButton.click();
+ }
+
+ public boolean isSongPlaying(){
+    WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+    return soundBar.isDisplayed();
+ }
+}
+
+
+
